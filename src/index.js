768d37b2ff459b30a40a2e79a98f9728221336d5
socratone <gim2origin@gmail.com>
2020-08-29 19:16:12 +0900

    Trim code

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import _ from 'lodash';
 
 function component() {
   const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+  element.innerHTML = _.join(['Hello', 'world'], ' ');
   return element;
 }
 
-document.body.appendChild(component());
+document.body.append(component());
