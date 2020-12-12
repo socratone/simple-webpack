63b5994951a96df18dacadc6ef717faac46e1777
socratone <gim2origin@gmail.com>
2020-12-12 20:14:44 +0900

    webpack-dev-server 설정

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import _ from 'lodash';
-
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'world'], ' ');
-  return element;
-}
-
-document.body.append(component());
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,13 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  target: 'web',
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000,
+    open: true, // 자동으로 브라우저를 띄운다.
   },
 };
